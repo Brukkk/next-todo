090d3db1d6a5b37fc0560070aa467560fdf22063
Bruno Torrabadella <bruktorra@gmail.com>
2023-07-04 17:52:09 -0300

    new page style

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 export default function Page() {
     return(
-        <h1>New</h1>
+        <>
+            <h1 className='text-2xl mb-4'>New</h1>
+            <form className="flex gap-2 flex-col">
+                <input
+                    type="text"
+                    name="title"
+                    className="border bg-transparent border-slate-300 rounded px-2 py-1 outline-none focus-within:border-slate-100"
+                />
+                <div className="flex gap-2 justify-end">
+                    <Link className='border border-slate-300 px-2 py-1 rounded hover:bg-slate-500 focus-within:bg-slate-500 outline-none' href="..">Cancel</Link>
+                    <button className='border border-slate-300 px-2 py-1 rounded hover:bg-slate-500 focus-within:bg-slate-500 outline-none' type="submit">Create</button>
+                </div>
+            </form>
+        </>
     )
 }
