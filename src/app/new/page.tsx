19465d995bd7e78b8f0048ae1f5358c6b05aa9fb
Bruno Torrabadella <bruktorra@gmail.com>
2023-07-04 18:06:54 -0300

    server actions

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,10 +1,24 @@
+import { prisma } from "@/db";
+import { redirect } from "next/navigation";
 import Link from "next/link";
+
+async function createTodo(data: FormData){
+    "use server"
+    const title = data.get("title")?.valueOf()
+
+    if (typeof title !== "string" || title.length === 0) {
+        throw new Error("Invalid title")
+    }
+    
+    await prisma.todo.create({ data: { title, complete: false }})
+    redirect("/")
+}
 
 export default function Page() {
     return(
         <>
             <h1 className='text-2xl mb-4'>New</h1>
-            <form className="flex gap-2 flex-col">
+            <form action={createTodo} className="flex gap-2 flex-col">
                 <input
                     type="text"
                     name="title"
