d683e4b7b6c7176650f1e40db6ff13b3788b74c3
Bruno Torrabadella <bruktorra@gmail.com>
2023-07-04 17:28:57 -0300

    todos & todo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from 'next/image'
+import { TodoItem } from '@/components/TodoItem'
+import { prisma } from '@/db'
 import Link from 'next/link'
 
-export default function Home() {
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+export default async function Home() {
+  const todos = await getTodos()
   return (
-    <header className='flex gap-1 justify-between items-center'>
-      <h1 className='text-2xl'>Todo</h1>
-      <Link className='border border-slate-300 px-2 py-1 rounded hover:bg-slate-500 focus-within:bg-slate-500 outline-none' href={'/new'}>New</Link>
-    </header>
+    <>
+      <header className='flex gap-1 justify-between items-center'>
+        <h1 className='text-2xl'>Todo</h1>
+        <Link className='border border-slate-300 px-2 py-1 rounded hover:bg-slate-500 focus-within:bg-slate-500 outline-none' href={'/new'}>New</Link>
+      </header>
+      <ul className='pl-4 pt-2'>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </ul>
+    </>
+
   )
 }
